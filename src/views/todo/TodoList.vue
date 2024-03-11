4b596e5ac5d661ab6ec762b0f82d576f3305955b
kooojidevman <tkkjbiz@gmail.com>
2024-03-11 09:22:42 +0900

    削除機能実装

--- src/views/todo/TodoList.vue
+++ src/views/todo/TodoList.vue
@@ -10,14 +10,23 @@ const onClickButtonToEdit = (todoId: number): void => {
   router.push({ name: 'TodoEdit', params: { id: todoId } })
 }
 const onClickButtonToDelete = (todoId: number): void => {
-  router.push({ name: 'TodoDelete', params: { id: todoId } })
+  todoList.delete(todoId);
 }
 </script>
 
 <template>
   <section style="text-align: center">
-    <h1 style="margin: 20px 0">TODOリスト</h1>
-    <input type="text" class="text-gray-900" />
+    <h1 style="margin: 20px">TODOリスト</h1>
+    <div style="margin: 20px">
+      <input type="text" class="text-gray-900" />
+      <button
+        type="button"
+        class="bg-gradient-to-r from-gray-100 to-grau-800 hover:bg-gradient-to-l text-white rounded px-4 py-2"
+        style="margin-left: 10px;"
+      >
+        検索
+      </button>
+    </div>
     <ul style="text-align: center; margin-bottom: 20px">
       <li v-for="[id, todo] in todoList" v-bind:key="id">
         <RouterLink v-bind:to="{ name: 'TodoDetail', params: { id: id } }">
